257680c37a5e24cc0faef20eeb6fe88a769f4c9c
Alper Mencek <alpmencek@gmail.com>
2026-04-27 19:23:18 -0700

    Created dashboard, loan-details and loan pages, updated navigation, setup dashboard

--- loandash-client/src/app/app.routes.ts
+++ loandash-client/src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+    },
+    {  
+        path: 'dashboard',
+        loadComponent: () => import('./pages/dashboard/dashboard').then(m => m.Dashboard)
+    },
+    {
+        path: 'loans',
+        loadComponent: () => import('./pages/loans/loans').then(m => m.Loans)
+    },
+    {
+        path: 'loans/:id',
+        loadComponent: () => import('./pages/loan-detail/loan-detail').then(m => m.LoanDetail)
+    },
+    {
+        path: '**',
+        redirectTo: 'dashboard'
+    }
+
+];
---
+++ loandash-client/src/app/pages/loan-detail/loan-detail.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loan-detail',
+  imports: [],
+  templateUrl: './loan-detail.html',
+  styleUrl: './loan-detail.scss',
+})
+export class LoanDetail {}
